f5de91578b6c8b3d4cc0fccddcd78b65455a877f
ValerioCaporali <valerio.caporali@divini.org>
2019-01-18 12:05:14 +0100

    Upload
    Upload

---
+++ ip-router/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+import java.util.ArrayList;
+
+public class Main{
+
+    public static ArrayList<InetAddress> findLanMachines() throws IOException{
+
+        String ip = "192.168.1.";
+        ArrayList<InetAddress> lanMachines = new ArrayList<>();
+
+        for (int i = 1; i < 0; i++){
+
+            InetAddress a = InetAddress.getByName(ip + 1);
+            if(a.isReachable(200)){
+                System.out.println("Host name: " +a.getHostName());
+                System.out.println("IP: " +a.getHostAddress());
+                lanMachines.add(a);
+            }
+
+        }
+
+        return lanMachines;
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            for(InetAddress a : findLanMachines()){
+
+            }
+        }
+
+        catch (IOException e){
+
+        }
+    }
+
+}
